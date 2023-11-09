0893fa7fd764ec38af462d8be1c3562cb15291c2
qadar omar <omarqadar3@gmail.com>
2023-11-08 21:01:16 -0800

    done with accessing elements

--- accessing.js
+++ accessing.js
@@ -1,5 +1,20 @@
 // Change the text of the "Seattle Weather" header to "February 10 Weather Forecast, Seattle"
 
+const weatherHeader = document.querySelector("#weather-head");
+weatherHeader.textContent = "February 10 Weather Forecast, Seattle";
+
 // Change the styling of every element with class "sun" to set the color to "orange"
 
+const sun = document.getElementsByClassName("sun");
+
+for (let i = 0; i < sun.length; i++) {
+  sun[i].style.color = "orange";
+}
+
+console.log(sun);
+
 // Change the class of the second <li> from to "sun" to "cloudy"
+
+const cloudy = document.getElementsByClassName("sun");
+
+sun[0].className = "cloudy";
