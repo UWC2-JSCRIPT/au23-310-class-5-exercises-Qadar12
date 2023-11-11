e07ff6cd32b5db1412af0a6904e4a87b4ab4c23a
qadar omar <omarqadar3@gmail.com>
2023-11-10 21:18:48 -0800

    finding the x and y

--- wheresThePointer.js
+++ wheresThePointer.js
@@ -1,3 +1,10 @@
 // Attach one listener that will detect clicks on any of the <td>
 // elements.  Should update that element's innerHTML to be the
 // x, y coordinates of the mouse at the time of the click
+
+const tBodyElement = document.getElementsByTagName("tbody")[0];
+
+tBodyElement.addEventListener("click", (e) => {
+  let tr = e.target;
+  tr.innerText = `X:${e.clientX} Y:${e.clientY}`;
+});
