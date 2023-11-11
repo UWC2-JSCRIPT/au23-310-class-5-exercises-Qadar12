bd0b42b15cf6a03278d7ea55884123361d116de1
qadar omar <omarqadar3@gmail.com>
2023-11-10 20:33:12 -0800

    Traversing the DOM up down left right

--- traversing.js
+++ traversing.js
@@ -1,11 +1,30 @@
 // Given the <body> element as variable body,
 // access the <main> node and log to the console.
-const body = document.querySelector('body');
+const body = document.querySelector("body");
+
+const bodyMain = body.children[0];
+console.log(bodyMain);
 
 // Given the <ul> element as variable ul,
 // access <body>  and log to the console.
-const ul = document.querySelector('ul');
+const ul = document.querySelector("ul");
+
+const parentMain = ul.parentElement;
+
+const grandBody = parentMain.parentElement;
+
+console.log(grandBody);
 
 // Given the <p> element as var p,
 // access the 3rd <li>  and log to the console.
-const p = document.querySelector('p');
+// start with p, go to the parent main, move down to ul element, then the 3rd
+
+const p = document.querySelector("p");
+
+const toTheParent = p.parentElement;
+
+const ulEm = toTheParent.children[1];
+
+const thirdUl = ulEm.children[2];
+
+console.log(thirdUl);
