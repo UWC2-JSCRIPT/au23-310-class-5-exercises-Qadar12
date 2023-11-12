6230a86e0866a93bf34c6245dc78552bb32a5bb5
qadar omar <omarqadar3@gmail.com>
2023-11-11 20:18:44 -0800

    start and stop events

--- stoppingBehavior.js
+++ stoppingBehavior.js
@@ -1,10 +1,26 @@
 // Do not change
-document.getElementById('cat').addEventListener('click', () => {
-  alert('meow!');
+document.getElementById("cat").addEventListener("click", () => {
+  alert("meow!");
 });
 
 // When clicked, "More info" link should alert "Here's some info"
 // instead of going to a new webpage
 
+moreInfo = document.getElementById("more-info");
+
+moreInfo.addEventListener("click", (event) => {
+  // Prevent the default click event
+  event.preventDefault();
+  alert("Here's some info");
+});
+
 // When the bark button is clicked, should alert "Bow wow!"
 // Should *not* alert "meow"
+
+dawg = document.getElementById("dog");
+
+dawg.addEventListener("click", (event) => {
+  // Prevent the event from propagating
+  event.stopPropagation();
+  alert("Bow wow");
+});
